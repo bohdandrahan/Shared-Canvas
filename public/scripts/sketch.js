ea7f4bc56c3596f2e1ab4aa859365d2b7453cf37
bohdandrahan <bohdan.drahan@gmail.com>
2019-01-26 15:29:31 -0500

    add connections

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -4,15 +4,17 @@ function setup() {
 	let h = 400
 	let w = 400
 	socket = io.connect('http://localhost:3000')
+  socket.on('mouse', newDrawing);
   createCanvas(h, w);
   background(51);
   clr = random(360)
+  noStroke()
 }
 
 
-function displayDot(x, y, color){
+function displayDot(x, y, color, color2 = 100){
 	colorMode(HSB)
-	fill(color, 100, 100)
+	fill(color, 100, color2)
 	ellipse(x, y, 10)
 	colorMode(RGB)
 }
@@ -22,11 +24,7 @@ function draw() {
 
 function mouseDragged() {
 	clr += 1
-	if (clr < 0){
-		clr = 360 - clr
-	} else if(clr > 360){
-		clr = clr % 360
-	}
+	clr = upgradeColor(clr)
 	let data = {
 		x: mouseX,
 		y: mouseY,
@@ -36,4 +34,16 @@ function mouseDragged() {
 	console.log('sending:', mouseX +',', mouseY +',', clr)
 	noStroke()
 	displayDot(mouseX, mouseY, clr)
+}
+function newDrawing(data){
+	data.color = upgradeColor(data.color)
+	displayDot(data.x, data.y, data.color, 30)
+}
+function upgradeColor(c){
+	if (c < 0){
+		c = 360 - c
+	} else if(c > 360){
+		c = c % 360
+	}
+	return c
 }
--- server.js
+++ server.js
@@ -16,7 +16,9 @@ io.sockets.on('connection', newConnection)
 function newConnection(socket){
 	console.log('connection:',	socket.id);
 	socket.on('mouse', mouseMsg);
+	
 	function mouseMsg(data) {
+		socket.broadcast.emit('mouse', data)
 		console.log(data)
 	}
 }
